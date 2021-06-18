8eede38b5f97d698e63ea4e2ed161b8346bb560b
hassam <malik.hassam531@gmail.com>
2021-06-18 12:43:51 +0500

    day clanedar complete

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -3,17 +3,55 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import {Link} from "react-router-dom";
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 function Calendar (){
-    const handlDateClick=(dateClickInfo:any )=>{
-console.log(dateClickInfo.dateStr);
-alert(dateClickInfo.dateStr);
+
+  const handleDateSelect = (selectInfo) => {
+        let title = prompt('Please enter a new title for your event')
+        let calendarApi = selectInfo.view.calendar
+
+        calendarApi.unselect() // clear date selection
+
+        if (title) {
+            calendarApi.addEvent({
+                title,
+                start: selectInfo.startStr,
+                end: selectInfo.endStr,
+                allDay: selectInfo.allDay
+            })
+        }
     }
+
+    const confirm = ()=>{
+
+    }
+
+  const handleEventClick = (clickInfo) => {
+        if (confirm
+        (`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+            clickInfo.event.remove()
+        }
+    }
+
+
     return(
         <>
      <FullCalendar
-     plugins={[dayGridPlugin, interactionPlugin]}
-     dateClick={handlDateClick}
+         headerToolbar={{
+             left: 'prev,next today',
+             center: 'title',
+             right: 'dayGridMonth,timeGridWeek,timeGridDay'
+         }}
+         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+         initialView="dayGridMonth"
+         weekends={true}
+         editable={true}
+         selectable={true}
+         selectMirror={true}
+         dayMaxEvents={true}
+         select={handleDateSelect}
+         eventClick={handleEventClick}
      />
      <br/>
             <Link to="/apidata" className="Link">Register a new Account</Link>
